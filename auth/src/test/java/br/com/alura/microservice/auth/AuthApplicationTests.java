3e889fb9c49db8ebacc167fa0fe4e1882c4835dc
Leandro Neves <leandrosoares6@hotmail.com>
2021-08-05 13:10:11 -0300

    feature: adicionando servidor de autenticacao

---
+++ auth/src/test/java/br/com/alura/microservice/auth/AuthApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.alura.microservice.auth;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
